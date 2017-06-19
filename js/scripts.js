170988054b9f6c63d4dbdc3d4ca6111d6fe7eb56
Jacek <jacek.paszek@onet.pl>
2017-06-19 20:31:53 +0200

    loop modification

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 function drawTree (k) {
   for (var i = 0; i < k; i++) {
-    var line = '*';
-    for (var j = 0; j < i; j++) {
+    var line = '';
+    for (var j = 0; j <= i; j++) {
       line += '*';
-  	};
-  	console.log(line);
+    };
+    console.log(line);
   }
 }
 
-drawTree(3);
+drawTree(4);
